375b7d698274f016639465ee8170505acf9c3b23
Tendai Nyandoro <tnyandoro@gmail.com>
2020-11-16 01:52:10 +0200

    Add gdp

--- select_within_select.sql
+++ select_within_select.sql
@@ -4,3 +4,13 @@ FROM world
   WHERE population >
      (SELECT population FROM world
       WHERE name='Russia')
+
+---Gdp bigger than UK
+SELECT name 
+FROM world 
+WHERE continent = 'Europe' and ( (gdp/population) > 
+(SELECT gdp/population from world where name = 'United Kingdom'))
+
+
+---
+
