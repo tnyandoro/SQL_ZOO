91c46fcfb5b19f133b1faae3adf0e3e2c03a14d5
Tendai Nyandoro <tnyandoro@gmail.com>
2020-11-15 23:32:31 +0200

    Add the query

--- select_from_nobel.sql
+++ select_from_nobel.sql
@@ -1,12 +1,21 @@
 ---Winners from 1950
 SELECT yr, subject, winner
-  FROM nobel
- WHERE yr = 1950
+FROM nobel
+WHERE yr = 1950
 
  ---1962 Literature
- SELECT winner
-  FROM nobel
- WHERE yr = 1962
-   AND subject = 'Literature'
+SELECT winner
+FROM nobel
+WHERE yr = 1962
+AND subject = 'Literature'
 
-   ---Albert Einstein
+---Albert Einstein
+SELECT winner
+FROM nobel
+WHERE yr = 1921
+AND subject = 'Physics'
+
+---Albert Einstein
+SELECT yr, subject
+FROM nobel
+WHERE winner = 'Albert Einstein';
