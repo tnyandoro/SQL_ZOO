4aa0ed700e4fcded393cd8d487fcf1e0417e6cb5
Tendai Nyandoro <tnyandoro@gmail.com>
2020-11-15 23:31:42 +0200

    Add Einsten

--- select_from_nobel.sql
+++ select_from_nobel.sql
@@ -2,3 +2,11 @@
 SELECT yr, subject, winner
   FROM nobel
  WHERE yr = 1950
+
+ ---1962 Literature
+ SELECT winner
+  FROM nobel
+ WHERE yr = 1962
+   AND subject = 'Literature'
+
+   ---Albert Einstein
