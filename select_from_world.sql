55d28db372affd64162073cce3fcfa0ae6f9d188
Tendai Nyandoro <tendai1@hotmail.com>
2020-11-15 16:19:11 +0200

    Ad Per capita GDP query

--- select_from_world.sql
+++ select_from_world.sql
@@ -4,3 +4,8 @@ SELECT name, continent, population FROM world
 ---Large Countries
 SELECT name FROM world
 WHERE population >= 200000000
+
+---Per capita GDP
+SELECT name, GDP/population 
+FROM world
+WHERE population >= 200000000;
