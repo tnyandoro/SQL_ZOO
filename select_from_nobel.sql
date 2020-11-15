f09eae9e216899239e23d63df5fbb9b6a109aec1
Tendai Nyandoro <tnyandoro@gmail.com>
2020-11-16 00:48:50 +0200

    add chemistry and physics qeury

--- select_from_nobel.sql
+++ select_from_nobel.sql
@@ -49,3 +49,7 @@ FROM nobel
 WHERE winner LIKE 'john %';
 
 ---Chemistry and Physics from different years
+SELECT yr, subject, winner 
+  FROM nobel
+WHERE yr = 1980 AND subject = 'physics' 
+  OR yr = 1984 AND subject = 'chemistry' ;
