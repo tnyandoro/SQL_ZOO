cacf78d10702907a52be9333791f22ac600c740f
Tendai Nyandoro <tendai1@hotmail.com>
2020-11-15 18:17:28 +0200

    Add For South America show population in millions and GDP in billions both to 2 decimal places.

--- select_from_world.sql
+++ select_from_world.sql
@@ -29,3 +29,15 @@ SELECT name, population, area
 FROM world
 WHERE area > 3000000 OR population > 250000000
 
+---Exclusive OR (XOR). Show the countries that are big by area (more than 3 million) or big by population (more than 250 million) but not both. Show name, population and area.
+SELECT name, population, area
+FROM world
+WHERE area > 3000000 XOR population > 250000000
+
+---For South America show population in millions and GDP in billions both to 2 decimal places.
+SELECT name, ROUND(population/1000000.0,2),ROUND(GDP/1000000000.0,2)
+FROM world
+WHERE continent = 'South America';
+
+
+
