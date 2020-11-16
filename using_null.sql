00655035522dab37399d5aab1097355e71bf2185
Tendai Nyandoro <tnyandoro@gmail.com>
2020-11-16 13:51:55 +0200

    add case

--- using_null.sql
+++ using_null.sql
@@ -21,4 +21,32 @@ SELECT name, COALESCE(mobile,'07986 444 2266')
 FROM teacher;
 
 ---Use the COALESCE function and a LEFT JOIN to print the teacher name and department name.
+SELECT teacher.name, COALESCE(dept.name,'None')
+FROM teacher left join dept on teacher.dept=dept.id;
 
+---Use COUNT to show the number of teachers and the number of mobile phones.
+SELECT count(name),count(mobile)
+FROM teacher;
+
+---Use COUNT and GROUP BY dept.name to show each department and the number of staff.
+SELECT dept.name,count(teacher.name)
+FROM teacher right join dept on teacher.dept=dept.id
+group by dept.name;
+
+
+---Use CASE to show the name of each teacher followed by 'Sci' if the teacher is in dept 1 or 2 and 'Art' otherwise.
+SELECT name,
+  case when dept=1 or dept=2
+       then 'Sci'
+       else 'Art'
+  end
+from teacher;
+
+---SELECT name,
+  case when dept=1 or dept=2
+       then 'Sci'
+       when dept=3
+       then 'Art'
+       else 'None'
+  end
+from teacher;
