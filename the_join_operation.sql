c3440ed035ef58e97c5de3d9ae5ea16d81107f48
Tendai Nyandoro <tnyandoro@gmail.com>
2020-11-16 09:15:40 +0200

    modify Germany

--- the_join_operation.sql
+++ the_join_operation.sql
@@ -4,3 +4,11 @@ FROM goal
 WHERE teamid = 'GER';
 
 --Show id, stadium, team1, team2 for just game 1012
+SELECT  id, stadium, team1, team2
+FROM  game
+WHERE  id = 1012;
+
+---Modify it to show the player, teamid, stadium and mdate for every German goal.
+SELECT  goal.player, goal.teamid, game.stadium, game.mdate
+FROM  game JOIN goal ON (game.id = goal.matchid)
+WHERE  goal.teamid = 'GER';
