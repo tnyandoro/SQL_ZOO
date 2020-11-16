0e754777d41e1da04c2d3b7c14e5c5816587d84f
Tendai Nyandoro <tnyandoro@gmail.com>
2020-11-16 12:49:21 +0200

    self join

---
+++ self_join.sql
@@ -0,0 +1,4 @@
+---How many stops are in the database.
+SELECT count(id) FROM stops;
+
+---Find the id value for the stop 'Craiglockhart'
---
+++ using_null.sql
@@ -0,0 +1,1 @@
+---List the teachers who have NULL for their department.
