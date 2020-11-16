8d2e4165a884173bb902a1c2bb68d1f89af4971a
Tendai Nyandoro <tnyandoro@gmail.com>
2020-11-16 11:45:30 +0200

    Add the casablanca cast list

--- more_join_operations.sql
+++ more_join_operations.sql
@@ -8,8 +8,31 @@ SELECT id, title
 FROM movie
 WHERE title = 'Citizen Kane';
 
-List all of the Star Trek movies, include the id, title and yr (all of these movies include the words Star Trek in the title). Order results by year.
+---List all of the Star Trek movies, include the id, title and yr (all of these movies include the words Star Trek in the title). Order results by year.
 SELECT id, title, yr
 FROM movie
 WHERE title LIKE '%Star Trek%'
 order by yr;
+
+---id for actor Glenn Close
+SELECT id
+FROM actor
+WHERE name = 'Glenn Close'
+
+---id for Casablanca
+SELECT id
+FROM movie
+WHERE title = 'Casablanca'
+
+---Cast list for Casablanca
+SELECT id
+FROM movie
+WHERE title = 'Casablanca'
+
+---Obtain the cast list for 'Casablanca'.
+
+
+SELECT name 
+FROM actor 
+JOIN casting ON id = actorid 
+WHERE movieid = 11768;
