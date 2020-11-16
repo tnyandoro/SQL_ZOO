e41fbf981db1e8802fb754c5f76176afc9792f5b
Tendai Nyandoro <tnyandoro@gmail.com>
2020-11-16 08:28:15 +0200

    Add Give countries with continents

--- select_within_select.sql
+++ select_within_select.sql
@@ -30,3 +30,37 @@ AND population <
     WHERE name = 'Poland');
 
 ---Percentages of Germany
+SELECT name, concat(ROUND(100*population/(SELECT population FROM world WHERE name= 'Germany'),0),'%') As 'percentage'
+FROM world WHERE continent ='Europe';
+
+---Which countries have a GDP greater than every country in Europe? [Give the name only.] (Some countries may have NULL gdp values)
+SELECT name
+FROM world
+WHERE gdp > ALL (SELECT gdp
+ FROM world
+ WHERE continent= 'Europe' AND gdp > 0 ); 
+
+---Largest in each continent
+SELECT continent, name, area FROM world x
+WHERE area >= ALL
+  (SELECT area FROM world y
+      WHERE y.continent=x.continent
+          AND area>0);
+
+---List each continent and the name of the country that comes first alphabetical
+
+SELECT continent, name FROM world x
+  WHERE name <= ALL
+    (SELECT name FROM world y
+        WHERE y.continent = x.continent);
+
+---Find the continents where all countries have a population <= 25000000. Then find the names of the countries associated with these continents. Show name, continent and population.        
+SELECT name, continent, population
+FROM world 
+WHERE continent NOT IN
+     (SELECT DISTINCT continent
+      FROM world
+      WHERE population > 25000000);
+
+--- Some countries have populations more than three times that of any of their neighbours (in the same continent). Give the countries and continents.
+     
