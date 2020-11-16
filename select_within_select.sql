99c6ea9dab627364918bb1207fe7d1e48aeaeb3e
Tendai Nyandoro <tnyandoro@gmail.com>
2020-11-16 08:30:13 +0200

    query country and continent

--- select_within_select.sql
+++ select_within_select.sql
@@ -63,4 +63,7 @@ WHERE continent NOT IN
       WHERE population > 25000000);
 
 --- Some countries have populations more than three times that of any of their neighbours (in the same continent). Give the countries and continents.
-     
+SELECT name, continent FROM world x
+WHERE population > ALL
+	(SELECT 3*population FROM world y
+		WHERE y.continent=x.continent AND x.name <> y.name);     
