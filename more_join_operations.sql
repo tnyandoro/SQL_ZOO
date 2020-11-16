89a22b0c19fe3d8f90a82d923426215152382fec
Tendai Nyandoro <tnyandoro@gmail.com>
2020-11-16 11:35:35 +0200

    Add Start trek movies

--- more_join_operations.sql
+++ more_join_operations.sql
@@ -7,3 +7,9 @@ SELECT id, title
  SELECT yr
 FROM movie
 WHERE title = 'Citizen Kane';
+
+List all of the Star Trek movies, include the id, title and yr (all of these movies include the words Star Trek in the title). Order results by year.
+SELECT id, title, yr
+FROM movie
+WHERE title LIKE '%Star Trek%'
+order by yr;
